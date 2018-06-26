d3ee814851e3714a8b2ba2ce42c5bbb3af5a20e0
540KJ <jljqiuc@gmail.com>
2018-06-26 18:28:08 +1000

    Add files via upload
    Andrew Calibration Code for changing motor speeds easily.

---
+++ Andrew_Calibration_Code.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+Servo firstESC, secondESC, thirdESC, fourthESC; //Create as much as Servo object you want. You can controll 2 or more Servos at the same time
+
+int value = 1000; // set values you need to zero
+
+void setup() {
+  Serial.begin(9600);    // start serial at 9600 baud
+
+  firstESC.attach(9);    // attached to pin 9 I just do this with 1 Servo
+  secondESC.attach(8);
+  thirdESC.attach(7);
+  fourthESC.attach(6);
+}
+
+void loop() {
+
+//First connect your ESC WITHOUT Arming. Then Open Serial and follo Instructions
+  Serial.println(value);
+  if(Serial.available()) 
+    value = Serial.parseInt();    // Parse an Integer from Serial
+  firstESC.writeMicroseconds(value);
+  secondESC.writeMicroseconds(value);
+  thirdESC.writeMicroseconds(value);
+  fourthESC.writeMicroseconds(value);
+}
+
